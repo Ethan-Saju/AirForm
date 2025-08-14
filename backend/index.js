245fc84ac21331895448820db620f63b1b1cc583
Ethan Saju <ethansaju10@gmail.com>
2025-08-14 23:12:55 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
